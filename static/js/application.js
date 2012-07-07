274c01b33418c71c11a9e47332f24c7f811c900f
lacemaker <merlettaia@gmail.com>
2012-07-07 19:13:46 +0400

    added basic click handler

--- static/js/application.js
+++ static/js/application.js
@@ -7,10 +7,19 @@ for (var i = 0; i < size; i++){
 row = $('<div />', {id:'row_'+i.toString(), class: 'row'});
 row.appendTo(parent_div);
  for (var j = 0; j < size; j++) {
-    $('<div />', { id: 'cell_' + i.toString() + '_' + j.toString(), 
+    cell = $('<div />', { id: 'cell_' + i.toString() + '_' + j.toString(), 
   class:'cell column_' + j.toString()
-  }).appendTo(row);
+  });
+  cell.appendTo(row);
+  
+  img = $('<div />',{class: 'image-unknown'
+					  });
+					  img.bind('click', function(){
+					  $(this).clearStyle;
+					  $(this).attr('class', 'image-empty');
+					  });
+img.appendTo(cell);
 }
 }
- 
+
 }
