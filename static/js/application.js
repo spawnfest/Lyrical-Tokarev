8e2c209eca249e3b84c5c90e3a696b8a3cdaf441
lacemaker <merlettaia@gmail.com>
2012-07-07 18:08:46 +0400

    added basic game board generator

--- static/js/application.js
+++ static/js/application.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
-  
+  fieldFactory($('#game_field'), 10);
 });
+
+function fieldFactory(parent_div, size){
+for (var i = 0; i < size; i++){
+row = $('<div />', {id:'row_'+i.toString(), class: 'row'});
+row.appendTo(parent_div);
+ for (var j = 0; j < size; j++) {
+    $('<div />', { id: 'cell_' + i.toString() + '_' + j.toString(), 
+  class:'cell', text:'ff'
+  }).appendTo(row);
+}
+}
+ 
+}
